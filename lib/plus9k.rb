635a6eb4862773a07899ec1578d79b3e3649e053
Dennis Sivia <dennis.sivia@gmail.com>
2019-08-17 17:33:56 +0200

    Make message customizable
    Add MESSAGE action input to allow users of the action to customize the
    message the action is responding with.

--- lib/plus9k.rb
+++ lib/plus9k.rb
@@ -2,22 +2,22 @@ require "octokit"
 require "json"
 
 class Plus9k
-  def initialize(token:, event_path:, comment: nil)
+  def initialize(token:, event_path:, message: nil)
     raise "Invalid or missing token" if token.to_s.length.zero?
 
     @event_path = event_path
     @payload = JSON.parse(File.read(event_path), symbolize_names: true)
     @client = Octokit::Client.new(:access_token => token)
-    @comment = comment || default_comment
+    @message = message || default_message
   end
 
-  def default_comment
-    <<~COMMENT
+  def default_message
+    <<~MESSAGE
       Thanks for supporting this discussion by sharing your opinion. :heart:
       Did you know? Dedicated `+1`-comments can make it hard to follow the discussion.
-      Sharing your support via emoji reactions on comments avoids that problem and helps us get a complete picture
-      of everybody's opinion. Make sure to use a reaction next time to make sure your vote is not lost.
-    COMMENT
+      Sharing your support via emoji reactions on comments avoids that problem and helps us get a complete picture of everybody's opinion.
+      Make sure to use a reaction next time to make sure your vote is not lost.
+    MESSAGE
   end
 
   def run()
@@ -47,7 +47,7 @@ class Plus9k
   end
 
   def reply(repo, issue_id)
-    puts "Adding comment: #{repo}/#{issue_id}"
-    @client.add_comment(repo, issue_id, @comment)
+    puts "Adding message: #{repo}/#{issue_id}"
+    @client.add_comment(repo, issue_id, @message)
   end
 end
